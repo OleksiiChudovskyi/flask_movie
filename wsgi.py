69e75d7d8a98bbb21e3737a1e940d55f4139f002
Oleksii <oleksii.chudovskyi@gmail.com>
2026-03-13 19:41:47 +0200

    Init GitHub Actions

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # app.run()
-    app.run(host="127.0.0.1", port=5000)
+    app.run()
+    # app.run(host="127.0.0.1", port=5000)
     # app.run(debug=True, host="127.0.0.1", port=5000)
