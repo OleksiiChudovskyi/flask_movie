2c4e8cb5653fc49143c4b7b4fc252593908d5716
Oleksii <oleksii.chudovskyi@gmail.com>
2026-03-14 19:32:46 +0200

    wsgi1

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,6 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run()
-    # app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000)
     # app.run(debug=True, host="127.0.0.1", port=5000)
-    app.run(debug=True, host="0.0.0.0")                       # for Dockerfile.dev
+    # app.run(debug=True, host="0.0.0.0")                       # for Dockerfile.dev
