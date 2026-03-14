6d56a1f12505795a0ba56d1a3b5474e0f9b07300
Oleksii <oleksii.chudovskyi@gmail.com>
2026-03-14 21:11:48 +0200

    wf_fix testing config

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host="127.0.0.1", port=5000)
+    # app.run()
+    app.run(host="127.0.0.1", port=5000)                        # for TestingConfig
     # app.run(debug=True, host="127.0.0.1", port=5000)
     # app.run(debug=True, host="0.0.0.0")                       # for Dockerfile.dev
